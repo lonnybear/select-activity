5061de6877a388861fc371b6400a6db4fdbf06f5
Lonny Le <lonnyorlonnie@gmail.com>
2023-02-08 09:11:05 -0800

    added more select queries for select activity

--- query-structure.sql
+++ query-structure.sql
@@ -1,12 +1,15 @@
-SELECT TransactionDate, MYear, County, gen_random_uuid() 'hi, mom!', 2011
-FROM registration
-limit 2;
 
-select distinct myear from registration order by myear asc limit 1;
-
-select min(myear) from registration limit 1;
-
-select count(make) from registration where myear < 1980; 
-select make, model from registration where myear < 1980;
-
+--how are BEVs distributed by county?
 select county, count(county) from registration where electrification = 'BEV (Battery Electric Vehicle)' group by county order by count desc;
+
+-- what is the model oldest year representated in the database? newest?
+oldest // select model, myear from registration order by myear asc;
+newest // select model, myear from registration order by myear desc;
+
+--number of tesla vehicles by model?
+select model, count(model) from registration where make = 'TESLA' group by model;
+
+--are mmost Ferraris red?
+select color, count(color) from registration where make = 'FERRARI' group by color;
+
+-- question how to check if they are filed as AWD FWD or RWD?
--- query.sql
+++ query.sql
@@ -1,7 +1,16 @@
- -- list of what manufacturers make
- SELECT make, model FROM registration;
- select count(model) from registration;
- select distinct model from registration where make = 'tesla' order by model;
- select model, count(model) from registration where make = 'TESLA' group by model;
- select color, count(color) from registration where make = 'FERRARI' group by color;
- 
+-- list of what manufacturers make
+SELECT make, model FROM registration;
+select count(model) from registration;
+select distinct model from registration where make = 'tesla' order by model;
+select model, count(model) from registration where make = 'TESLA' group by model;
+select color, count(color) from registration where make = 'FERRARI' group by color;
+SELECT TransactionDate, MYear, County, gen_random_uuid() 'hi, mom!', 2011
+FROM registration
+limit 2;
+
+select distinct myear from registration order by myear asc limit 1;
+
+select min(myear) from registration limit 1;
+
+select count(make) from registration where myear < 1980; 
+select make, model from registration where myear < 1980;
